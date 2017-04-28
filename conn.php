e11a7072b027b105b0f6d83511ffb2d096e39839
beidou566 <beidou566@qq.com>
2017-04-28 15:57:27 +0800

    2
    2

---
+++ conn.php
@@ -0,0 +1,15 @@
+﻿<?php
+	error_reporting(0); 
+	header('Content-Type:text/html;charset=utf-8');
+	
+	define('DB_HOST','127.0.0.1');
+	define('DB_USER','root');
+	define('DB_PWD','111111');
+	define('DB_NAME','lvchaoeng');
+	
+	$conn = @mysql_connect(DB_HOST,DB_USER,DB_PWD) or die(header("Location: help.php"));
+	
+	mysql_select_db(DB_NAME) or die(header("Location: help.php"));
+	mysql_query('SET NAMES UTF8') or die('字符集设置错误'.mysql_error());
+	date_default_timezone_set('PRC'); 
+?>
---
+++ index.php
@@ -0,0 +1,72 @@
+<?php
+
+$to = "test@163.com"; //收件人  
+$subject = "Test"; //主题  
+$message = "This is a test mail!"; //正文  
+mail($to,$subject,$message); 
+
+
+require_once 'conn.php';
+require_once "email.class.php";
+
+$name=$_POST["name"];
+error_log('name:'.$name, 3, 'info.log');
+
+if ($name != "") {
+	$tel=$_POST["tel"];
+	$school=$_POST["school"];
+	$age=$_POST["age"];
+	
+error_log('tel:'.$tel."\r\n", 3, 'info.log');
+error_log('school:'.$school."\r\n", 3, 'info.log');
+error_log('age:'.$age."\r\n", 3, 'info.log');
+
+	//写入数据
+	$query = "INSERT INTO studentlist (
+	name,
+	tel,
+	school,
+	age,
+	modifytime
+	) VALUES (
+	'$name',
+	'$tel',
+	'$school',
+	'$age',
+	now()
+	)";
+	@mysql_query($query) or die('新增错误：'.mysql_error());
+
+	error_log('query:'.$query."\r\n", 3, 'info.log');
+
+	$idnow = mysql_insert_id(); 
+	error_log('idnow:'.$idnow."\r\n", 3, 'info.log');
+	  
+	if ($idnow == 0) {  
+	
+    //******************** 配置信息 ********************************
+	$smtpserver = "smtp.126.com";//SMTP服务器
+	$smtpserverport =25;//SMTP服务器端口
+	$smtpusermail = "new2008oh@126.com";//SMTP服务器的用户邮箱
+	$smtpemailto = $_POST['toemail'];//发送给谁
+	$smtpuser = "new2008oh";//SMTP服务器的用户帐号
+	$smtppass = "这里是密码";//SMTP服务器的用户密码
+	$mailtitle = $_POST['title'];//邮件主题
+	$mailcontent = "<h1>".$_POST['content']."</h1>";//邮件内容
+	$mailtype = "HTML";//邮件格式（HTML/TXT）,TXT为文本邮件
+	//************************ 配置信息 ****************************
+	$smtp = new smtp($smtpserver,$smtpserverport,true,$smtpuser,$smtppass);//这里面的一个true是表示使用身份验证,否则不使用身份验证.
+	$smtp->debug = false;//是否显示发送的调试信息
+	$state = $smtp->sendmail($smtpemailto, $smtpusermail, $mailtitle, $mailcontent, $mailtype);
+	
+		exit(json_encode(1)); //返回1表示注册失败  
+	} else {    
+		exit(json_encode(0)); //返回0表示注册成功  
+	}  
+	
+	
+
+
+}
+?>
+
