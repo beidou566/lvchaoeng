770d4af4db88a4f8097afb11ee06241fe3f7085b
beidou566 <beidou566@qq.com>
2017-05-04 12:39:03 +0800

    1
    1

--- public/conn.php
+++ public/conn.php
@@ -2,9 +2,9 @@
 	error_reporting(0); 
 	header('Content-Type:text/html;charset=utf-8');
 	
-	define('DB_HOST','127.0.0.1');
-	define('DB_USER','root');
-	define('DB_PWD','111111');
+	define('DB_HOST','139.196.198.146');
+	define('DB_USER','cater');
+	define('DB_PWD','J7NpYy9nyZEjCffH');
 	define('DB_NAME','lvchaoeng');
 	
 	$conn = @mysql_connect(DB_HOST,DB_USER,DB_PWD) or die(header("Location: help.php"));
